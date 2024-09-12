96970169e81efe294f2dcde48d64d0db08003b09
rutvikgit276 <rutvikp276@gmail.com>
2024-09-12 21:23:10 +0530

    Add files via upload

---
+++ String programs/mod3.1_lengthstring(strings).c
@@ -0,0 +1,16 @@
+// 1. Write a program in C to find the length of a string without using library 
+//		functions.
+
+#include <stdio.h>
+main() 
+{
+    char str[30];
+    int length = 0,i;
+
+    printf("Enter a string: ");
+    scanf("%s", str);  
+    for (i = 0; str[i] != '\0'; i++) {
+        length++;
+    }
+    printf("Length of the string is: %d", length);
+}
---
+++ String programs/mod3.2_seperatechar(strings).c
@@ -0,0 +1,16 @@
+// 2. Write a program in C to separate individual characters from a string. 
+
+#include <stdio.h>
+main() 
+{
+	int i;
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);  
+
+    printf("Individual characters in the string are:\n");
+    for (i = 0; str[i] != '\0'; i++) 
+	{
+        printf("%c\n", str[i]);  
+    }
+}
